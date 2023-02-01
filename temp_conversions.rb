e5ccbedc3f71bf9417e431313d8efd4e6b0d5b41
Omar <osb@andrew.cmu.edu>
2023-02-01 18:48:15 +0300

    fixed non integer entry bug

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,6 +1,7 @@
 
 # making the covert method
 def convert(temp)
+    return "Temperature must be an integer" unless temp.class == Integer
     return ((temp-32)*5)/9
 end
 
@@ -9,3 +10,6 @@ end
 puts convert(32)          
 puts convert(50)          
 puts convert(212)
+
+# edge case test
+puts convert("zero")
