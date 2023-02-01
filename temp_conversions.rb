a2f964acc5ca08abf80c2a223725b1eea173e2fc
Omar <osb@andrew.cmu.edu>
2023-02-01 20:02:19 +0300

    added a new branch with an if statement

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,13 +1,19 @@
 
 # making the covert method
-def convert(temp)
+def convert(temp, measure = "F")
     return "Temperature must be an integer" if temp.class != Integer
-    return "Temperature below Absolute Zero" if temp < -474 
-    return ((temp-32)*5)/9
+
+    if measure == "F"
+        return "Temperature below Absolute Zero" if temp < -474 
+        return ((temp-32)*5)/9
+    else 
+        return (temp*1.8)+32
+    end
+
 end
 
 
-# test cases
+# F test cases
 puts convert(32)          
 puts convert(50)          
 puts convert(212)
@@ -15,3 +21,9 @@ puts convert(212)
 # edge case test
 puts convert("zero")
 puts convert(-500)
+
+# C test Cases
+puts convert(0, "C")
+puts convert(10, "C")
+puts convert(100, "C")
+puts convert(-280, "C")
